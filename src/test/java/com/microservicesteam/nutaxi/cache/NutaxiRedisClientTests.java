f0d6896186785ccf071ca580fde53fc4382f5a1e
szogi <szogibalu@gmail.com>
2016-07-27 21:09:08 +0200

    MS-0 Fixed broken build

--- src/test/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClientTests.java
+++ src/test/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClientTests.java
@@ -1,20 +1,17 @@
 package com.microservicesteam.nutaxi.cache;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
+import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.embedded.RedisServer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = NutaxiRedisClient.class)
@@ -23,28 +20,22 @@ public class NutaxiRedisClientTests {
 	@Autowired
 	private ApplicationContext applicationContext;
 
+	private static RedisServer redisServer;
+
+	@BeforeClass
+	public static void init() throws Exception {
+		redisServer = new RedisServer(6379);
+		redisServer.start();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		redisServer.stop();
+	}
+
 	@Test
 	public void contextLoads() {
 		assertNotNull(applicationContext);
-	}
-
-	@Configuration
-	static class RedisTestConfiguration {
-
-		@Bean
-		public RedisSerializer<?> defaultRedisSerializer() {
-			return mock(RedisSerializer.class);
-		}
-
-		@Bean
-		public RedisConnectionFactory connectionFactory() {
-			RedisConnectionFactory factory = mock(RedisConnectionFactory.class);
-			RedisConnection connection = mock(RedisConnection.class);
-
-			when(factory.getConnection()).thenReturn(connection);
-
-			return factory;
-		}
 	}
 
 }

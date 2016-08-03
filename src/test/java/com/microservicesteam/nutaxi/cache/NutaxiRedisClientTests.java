aa77cbdab95d758f9d4185a87bd73d630f15e1ed
szogi <szogibalu@gmail.com>
2016-08-03 21:00:55 +0200

    MS-0 Polishing

--- src/test/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClientTests.java
+++ src/test/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClientTests.java
@@ -16,7 +16,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -31,7 +31,7 @@ import com.microservicesteam.nutaxi.cache.model.Route;
 import redis.embedded.RedisServer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = NutaxiRedisClient.class)
+@SpringBootTest(classes = NutaxiRedisClient.class)
 @WebAppConfiguration
 public class NutaxiRedisClientTests {
 

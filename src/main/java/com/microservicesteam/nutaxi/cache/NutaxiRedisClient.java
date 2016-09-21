d4d8f1d28b325f97f080ae4cf74e3335fb2451fc
szogi <szogibalu@gmail.com>
2016-09-21 19:33:23 +0200

    Polishing

--- src/main/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClient.java
+++ src/main/java/com/microservicesteam/nutaxi/cache/NutaxiRedisClient.java
@@ -1,10 +1,10 @@
 package com.microservicesteam.nutaxi.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.microservicesteam.nutaxi.cache.model.Route;
@@ -12,15 +12,13 @@ import com.microservicesteam.nutaxi.cache.model.Route;
 @SpringBootApplication
 public class NutaxiRedisClient {
 
-    @Bean
-    public RedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory();
-    }
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     @Bean
     public RedisTemplate<Long, Route> template() {
         RedisTemplate<Long, Route> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
+        template.setConnectionFactory(connectionFactory);
 
         return template;
     }
